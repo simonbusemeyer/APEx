c0f8a9a3213efcee6c30c52d3569c999750a5cc4
Simon Busemeyer <simon@Seoul-3.local>
2026-06-19 00:33:42 +0200

    turned off nessie injection

--- Internship_NetSurvival/2_Code/current/main_batch.R
+++ Internship_NetSurvival/2_Code/current/main_batch.R
@@ -6,7 +6,8 @@ library(relsurv)
 library(future.apply)
 library(data.table)
 
-source("current/functions/generate_dataModified_ng.R")
+source("fixes for later/generate_dataModified_ng_corrections.R")
+#source("current/functions/generate_dataModified_ng.R")
 source("current/functions/analyze_one_ng.R")
 source("current/functions/compute_metrics.R")
 #source("current/functions/rltf_calibration.R")
@@ -15,37 +16,43 @@ source("current/functions/nessie.R")
 
 # Global Parameters
 n_patients <- 2000
-age_option <- "LuoTrunc"
+age_option <- "Luo"
 beta_age <- 0.02
 beta_sex <- 0
 year.start_min <- 2008
 year.start_max <- 2010
 prop_female <- 0
-N_files <- 10
+N_files <- 1000
 
-lambdas_to_run <- c(0.004, 0.005, 0.007, 0.01, 0.014, 0.019, 0.03, 0.04, 0.055, 0.07, 0.10, 0.30) # use for Luo/LuoTrunc
+
+lambdas_to_run <- c(0.014) 
+
+#lambdas_to_run <- c(0.004, 0.005, 0.007, 0.01, 0.014, 0.019, 0.03, 0.04, 0.055, 0.07, 0.10, 0.30) # use for Luo/LuoTrunc
 
 # Calculate max_time
-cat("Calculating dynamic max_time based on population expected survival...\n")
-max_time <- determine_max_time(
-  n_patients = n_patients,
-  age_option = age_option,
-  prop_female = prop_female,
-  year.start_min = year.start_min,
-  year.start_max = year.start_max
-)
+# cat("Calculating dynamic max_time based on population expected survival...\n")
+# max_time <- determine_max_time(
+#   n_patients = n_patients,
+#   age_option = age_option,
+#   prop_female = prop_female,
+#   year.start_min = year.start_min,
+#   year.start_max = year.start_max
+# )
+
+max_time <- 10
+
 max_time_days <- max_time * 365.241
 cat(sprintf("=> Dynamic max_time set to: %d years\n\n", max_time))
-# Run the calibration engine to dynamically generate the scenarios dataframe
+ #Run the calibration engine to dynamically generate the scenarios dataframe
 scenarios <- calibrate_censoring_grid(
   lambdas = lambdas_to_run,
   n_patients = n_patients,
-  max_time = max_time,
-  age_option = age_option,
+   max_time = max_time,
+   age_option = age_option,
   beta_age = beta_age,
-  target_censoring = 0.30, # target random loss to follow-up
-  n_pilots = 3        # Adjust based on variance
-)
+   target_censoring = 0.30, # target random loss to follow-up
+   n_pilots = 2        # Adjust based on variance
+ )
 
 # Display the calculated scenarios to verify before main execution
 print("=== Final Calibrated Scenarios ===")
