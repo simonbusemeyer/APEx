04a3696e3b6ad158b8f880370da9ea6ea864fbef
Simon Busemeyer <simon@Seoul-3.local>
2026-06-19 08:58:54 +0200

    reverted unconditional metrics and added absolute bias

--- Internship_NetSurvival/2_Code/current/functions/compute_metrics.R
+++ Internship_NetSurvival/2_Code/current/functions/compute_metrics.R
@@ -21,37 +21,48 @@ compute_metrics <- function(results_list, lambda_val, borne_a_val){
     time_t = times_eval,
     
     # New columns for absolute counts
-    n_total = numeric(n_times),
-    n_success = numeric(n_times),
+    # n_total = numeric(n_times),
+    # n_success = numeric(n_times),
+    # 
+    # # New columns for reliability
+    # convergence_rate = numeric(n_times),
+    # failure_rate = numeric(n_times),
+    # 
+    # # New columns for bifurcated metrics
+    # bias_conditional = numeric(n_times),
+    # rmse_conditional = numeric(n_times),
+    # ecr_conditional = numeric(n_times),
+    # ecr_unconditional = numeric(n_times)
     
-    # New columns for reliability
-    convergence_rate = numeric(n_times),
-    failure_rate = numeric(n_times),
-    
-    # New columns for bifurcated metrics
-    bias_conditional = numeric(n_times),
-    rmse_conditional = numeric(n_times),
-    ecr_conditional = numeric(n_times),
-    ecr_unconditional = numeric(n_times)
+    absolute_bias = numeric(n_times),
+    bias = numeric(n_times),
+    rmse = numeric(n_times),
+    ecr = numeric(n_times)
   )
   
   # Calculate metrics by time t
   for (i in 1:n_times) {
     res_t <- all_res[all_res$time == times_eval[i],]
     
-    metrics$n_total[i] <- nrow(res_t)
-    metrics$n_success[i] <- sum(!is.na(res_t$diff))
+    metrics$absolute_bias[i] <- abs(mean(res_t$diff, na.rm = TRUE))
+    metrics$bias[i] <- mean(res_t$diff, na.rm = TRUE)
+    metrics$rmse[i] <- sqrt(mean(res_t$diff^2, na.rm = TRUE))
+    metrics$ecr[i] <- mean(res_t$covered, na.rm = TRUE)
     
-    metrics$convergence_rate[i] <- mean(!is.na(res_t$diff))
-    metrics$failure_rate[i] <- mean(is.na(res_t$diff))
-    
-    #Calculate metrics conditional on successful convergence
-    metrics$bias_conditional[i] <- mean(res_t$diff, na.rm = TRUE)
-    metrics$rmse_conditional[i] <- sqrt(mean(res_t$diff^2, na.rm = TRUE))
-    metrics$ecr_conditional[i] <- mean(res_t$covered, na.rm = TRUE)
-    
-    #Calculate Unconditional Coverage where NA is a failure to cover
-    metrics$ecr_unconditional[i] <- sum(res_t$covered %in% TRUE) / nrow(res_t)
+ #not computing conditional/unconditional metrics for now
+    # metrics$n_total[i] <- nrow(res_t)
+    # metrics$n_success[i] <- sum(!is.na(res_t$diff))
+    # 
+    # metrics$convergence_rate[i] <- mean(!is.na(res_t$diff))
+    # metrics$failure_rate[i] <- mean(is.na(res_t$diff))
+    # 
+    # #Calculate metrics conditional on successful convergence
+    # metrics$bias_conditional[i] <- mean(res_t$diff, na.rm = TRUE)
+    # metrics$rmse_conditional[i] <- sqrt(mean(res_t$diff^2, na.rm = TRUE))
+    # metrics$ecr_conditional[i] <- mean(res_t$covered, na.rm = TRUE)
+    # 
+    # #Calculate Unconditional Coverage where NA is a failure to cover
+    # metrics$ecr_unconditional[i] <- sum(res_t$covered %in% TRUE) / nrow(res_t)
   }
   
   return(metrics)
