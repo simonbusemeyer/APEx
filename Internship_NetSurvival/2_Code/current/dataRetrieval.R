11abbc6f77da510ec83ef529b264523c3c6d2707
Simon Busemeyer <simonbusemeyer@gmail.com>
2026-06-24 10:53:51 +0200

    formatting and here()

--- Internship_NetSurvival/2_Code/current/dataRetrieval.R
+++ Internship_NetSurvival/2_Code/current/dataRetrieval.R
@@ -1,13 +1,14 @@
+# dataRetrieval.R
 library(arrow)
 library(dplyr)
 
 # Maps the directory without loading it into RAM
 cohort_dataset <- arrow::open_dataset(
-  sources = "current/outputs/data", 
+  sources = here("current", "outputs", "data"), 
   format = "parquet"
 )
 
-# Performs the aggregation safely at the C++ level
+# Performs the aggregation at the C++ level
 lambda_verification <- cohort_dataset %>%
   group_by(lambda) %>%
   summarize(total_rows = n()) %>%
